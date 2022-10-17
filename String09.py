e638dc9ad7de74248dc648b902c4c5066019750a
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-17 10:35:18 +0500

    String09.py

--- String09.py
+++ String09.py
@@ -5,9 +5,9 @@ def main(x1,x2,x3):
         x1: int
         x2: int
         x3: int
-    Returns:
+    Returns:s
         str: return answer.
     """
     answer=(f"[{x1},{x2},{x3}]")
-    return str(answer)
-print (main(1,2,3)) 
+    return (answer)
+print (main(1,2,3))
